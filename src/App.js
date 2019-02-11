11d8694858d6c4b751ebfcd1fb862e6aa877d490
Prosenjit Manna <prosenjit@itobuz.com>
2019-02-11 20:32:53 +0530

    contact list created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import faker from 'faker/index';
+
+import './App.scss';
 
 class App extends Component {
+  persons = Array(7).join(0).split(0).map((v, i) => i + 1).map((item) => {
+    return {
+      id: item,
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      address: faker.address.streetAddress(),
+      bio: faker.lorem.sentence(),
+      image: faker.image.avatar()
+    }
+  });
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React Contact book</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID#</th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              this.persons.map(item => {
+                return (
+                <tr key={item.id}>
+                  <td width="20">{item.id}</td>
+                  <td width="100">
+                  <img src={item.image} alt="{item.name}" width="50"/>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
+
     );
   }
 }
